8c9b390c0be2ae853a5ba112f0dcb1f229f26be7
Ricardo Benitez <benitezc.ricardo@gmail.com>
2020-11-02 18:19:42 -0600

    [Config] Adding build type to the build script
    Signed-off-by: Ricardo Benitez <benitezc.ricardo@gmail.com>

--- build.py
+++ build.py
@@ -3,17 +3,24 @@ import os
 import shutil
 import argparse
 
-cmakeCommand = ['cmake', '-G', 'Unix Makefiles', '-DCMAKE_BUILD_TYPE=Release', '..']
+cmakeCommand = ['cmake', '-G', 'Unix Makefiles', '-DCMAKE_BUILD_TYPE=', '..']
 makeCommand = ['make', '-j4']
 makeCleanCommand = ['make', 'clean']
 
+valid_build_type = ['debug', 'release']
+
 
 if __name__ == "__main__":
+    print("Python build script")
     parser = argparse.ArgumentParser(description='Python build script')
-    parser.add_argument('--force', '-f', action='store_const', dest='force', const=True,
+    parser.add_argument('-b', '--build', dest='build_type',
+        action='store', required=True, default='debug',
+        help="The build type to run: [" + ','.join(valid_build_type) + "]. Default=debug")
+    parser.add_argument('-f', '--force', action='store_const', dest='force', const=True,
         help='force the rebuild of the program')
-    parser.add_argument('--clean', '-c', action='store_const', dest='clean', const=True,
+    parser.add_argument('-c', '--clean', action='store_const', dest='clean', const=True,
         help='Clean the project and build directory')
+
     args = parser.parse_args()
 
     if (args.clean):
@@ -22,7 +29,7 @@ if __name__ == "__main__":
         except FileNotFoundError:
             pass
         os._exit(0)
-    print("Python build script")
+
     try:
         os.mkdir("compilation")
     except FileExistsError:
@@ -30,6 +37,12 @@ if __name__ == "__main__":
     os.chdir('./compilation')
     if args.force:
         result = subprocess.run(makeCleanCommand)
+    
+    if (args.build_type in valid_build_type):
+        cmakeCommand[-2] = cmakeCommand[-2] + args.build_type.upper()
+    else:
+        cmakeCommand[-2] = cmakeCommand[-2] + valid_build_type[0]
+
     result = subprocess.run(cmakeCommand)
     with open('error_output.txt', 'w') as fileHandler:
         result = subprocess.run(makeCommand, stderr=fileHandler)
